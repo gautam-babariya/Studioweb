319741ce9264ceaa5c08154782767006e46348b8
gautam <gautambabariya0125@gmail.com>
2024-03-05 00:02:59 +0530

    back

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const multer = require('multer');
 // cors code 
 var cors = require('cors')
 app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 // mongo connection..........................
 const connect = async () => {
