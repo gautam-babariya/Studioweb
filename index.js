d0ada5db91ab52ea05594f3819c93e5444431a11
gautam <gautambabariya0125@gmail.com>
2024-05-28 16:14:45 +0530

    avy error no hoy

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const path = require('path');
 const cors = require('cors');
 
 // cors code 
-// app.use(cors())
-//   app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-//     res.setHeader('Access-Control-Max-Age', 2592000);
-//     next();
-// });
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Max-Age', 2592000);
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
  
 
